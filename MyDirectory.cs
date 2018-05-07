101b0ea12f4efad6863a8a1ce30c291c74adfcf2
Maksym Havryliuk <maks.havryliuk@gmail.com>
2018-05-07 09:30:53 +0300

    Threading, the solution of the 1st task (FolderSize).

---
+++ MyDirectory.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+/**<remark>
+ * https://msdn.microsoft.com/ru-ru/library/system.io.fileinfo.length(v=vs.110).aspx
+ * Определить размер каталога на C# https://alekseygulynin.ru/razmer-kataloga-c/
+ * </remark> */
+
+namespace _20180403_Task1_FolderSize
+{
+    internal class MyDirectory
+    {
+        private double directorySize = 0.0d;            // Размер директории
+        private int startOfRange = 0, endOfRange = 9;   // Отсчет по 10 файлов на поток
+
+        internal void SizeOfDirectory(string pathToDirectory)
+        {
+            DirectoryInfo linkToDirectory = new DirectoryInfo(pathToDirectory);   // Создаем ссылку на директорию.
+            FileInfo[] fileInfo = linkToDirectory.GetFiles();                     // Получаем ссылку на каждый файл в заданной директории.
+
+            // В цикле пробегаемся по всем файлам директории и складываем их размеры
+            foreach (FileInfo file in fileInfo)
+            {
+                directorySize = directorySize + file.Length;              //Записываем размер файла в байтах
+            }
+
+            directorySize = Math.Round(directorySize / 1024 / 1024, 1);   // 1МБ = 1024 Байта * 1024 КБайта
+
+            Console.WriteLine(directorySize != 0
+                ? $"Размер директории {pathToDirectory} составляет: {directorySize} МБ"
+                : $"Каталог {pathToDirectory} пуст.");
+
+            Console.WriteLine($"Количество файлов в директории: {fileInfo.Length}\n");
+
+
+            //Создаем в цикле n потоков, каждый из которых друг за другом расчитывает размер 10 файлов
+            for (int i = 0; i < fileInfo.Length; i += 10)
+            {
+                Task task = Task.Run(() =>
+                {
+                    if (endOfRange > fileInfo.Length) endOfRange = startOfRange + fileInfo.Length % 10 - 1;
+
+                    for (int j = startOfRange; j <= endOfRange; j++)
+                    {
+                        directorySize += fileInfo[j].Length;
+                    }
+
+                    directorySize = Math.Round(directorySize / 1024 / 1024, 1);
+
+                    Console.WriteLine($"{Task.CurrentId} поток -> размер файлов с {startOfRange + 1} по {endOfRange + 1}: {directorySize} МБ");
+
+                    startOfRange += 10;
+                    endOfRange += 10;
+                });
+
+                task.Wait();      // Ожидать завершения задачи task
+                task.Dispose();   // Освободить задачу task
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+/**<remark>
+ * Calculate the folder size.
+ * Start n threads simultaneously.Each thread calculates the sum of size for the 10 files.
+ * Do this task using an array(or list) of threads working independently.
+ * Perform this task using task parallel library.
+ *
+ * Вычислите размер папки.
+ * Начать n потоков одновременно. Каждый поток вычисляет сумму размера для 10 файлов.
+ * Выполняйте эту задачу с использованием массива (или списка) потоков, работающих независимо.
+ * Выполните эту задачу, используя параллельную библиотеку задач.
+ * </remark> */
+
+namespace _20180403_Task1_FolderSize
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            const string pathToDirectory = @"W:\Soft";
+
+            try
+            {
+                MyDirectory myDirectory = new MyDirectory();
+                myDirectory.SizeOfDirectory(pathToDirectory);
+            }
+
+            // DirectoryNotFoundException - директория не найдена
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Внимание: проверьте путь к директории.");
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            // UnauthorizedAccessException - отсутствует доступ к файлу или папке
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Внимание: проверьте доступ к директории.");
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            // Во всех остальных случаях
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Внимание: Произошла ошибка!");
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
